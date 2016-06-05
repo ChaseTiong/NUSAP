1560033ea54bef4fb876368523028ccec5cafe44
chasexyaocong <tiongyaocong@gmail.com>
2016-06-06 01:02:29 +0800

    Implementation of stacked Bar

--- src/js/controllers/master-ctrl.js
+++ src/js/controllers/master-ctrl.js
@@ -26,49 +26,10 @@ function getUrlVars() {
 var userName;
 var studentId;
 
-(function() {
-    var app = angular.module('app', ['ngAnimate']);
-
-    app.animation('.slide-toggle', ['$animateCss', function($animateCss) {
-        return {
-            addClass: function(element, className, doneFn) {
-                if (className == 'ng-hide') {
-                    var animator = $animateCss(element, {                    
-                        to: {height: '0px', opacity: 0}
-                    });
-                    if (animator) {
-                        return animator.start().finally(function() {
-                            element[0].style.height = '';
-                            doneFn();
-                        });
-                    }
-                }
-                doneFn();
-            },
-            removeClass: function(element, className, doneFn) {
-                if (className == 'ng-hide') {
-                    var height = element[0].offsetHeight;
-                    var animator = $animateCss(element, {
-                        from: {height: '0px', opacity: 0},
-                        to: {height: height + 'px', opacity: 1}
-                    });
-                    if (animator) {
-                     return animator.start().finally(doneFn);
-                    }
-                }
-                doneFn();
-            }
-        };
-    }]);
-})();
-
-
 angular.module('NUSAP')
     .controller('MasterCtrl', ['$scope', '$cookieStore', '$http', '$location', MasterCtrl]);
 
 function MasterCtrl($scope, $cookieStore, $http, $location) {
-   
-  
     /**
      * Sidebar Toggle & Cookie Control
      */
