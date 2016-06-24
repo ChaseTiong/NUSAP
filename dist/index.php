131bccccb49f9cf187201609b8bb8efdde9d9e7a
ruiwen905 <ruiwen905@gmail.com>
2016-06-25 05:15:46 +0800

    acad-planner-ctrl.js update and tables.html , new stable version

--- dist/index.php
+++ dist/index.php
@@ -5,13 +5,18 @@
 		$token = $_GET['token'];
 	}	
 	$id = $_GET['id'];
+    $studentID = $_GET["netid"];
 	if(strcmp($id, "profile") == 0){
 		$url = "https://ivle.nus.edu.sg/api/Lapi.svc/Profile_View?APIKey=".$APIKey."&AuthToken=".$token;
-		$xml = file_get_contents("$url");
-		echo $xml;	
+		$json = file_get_contents("$url");
+		echo $json;	
 	} else if(strcmp($id, "checkToken") == 0){
 		$url = "https://ivle.nus.edu.sg/api/Lapi.svc/Validate?APIKey=".$APIKey."&Token=".$token;
-		$xml = file_get_contents("$url");
-		echo $xml;
-	}	
+		$json = file_get_contents("$url");
+		echo $json;
+	} else if (strcmp($id, "modTaken") == 0){
+        $url = "https://ivle.nus.edu.sg/api/Lapi.svc/Modules_Taken?APIKey=".$APIKey."&AuthToken=".$token."&StudentID=".$studentID;
+		$json = file_get_contents("$url");
+		echo $json;	
+    }
 ?>
---
+++ src/js/controllers/acad-planner-ctrl.js
@@ -0,0 +1,52 @@
+/**
+ * Alerts Controller
+ */
+var token;
+
+if(sessionStorage.getItem("token") == null){
+	token = getUrlVars()["token"];
+	sessionStorage.setItem("isValidToken",false);
+} else{
+	token = sessionStorage.getItem("token");
+	sessionStorage.setItem("isValidToken",true);
+}
+
+function getUrlVars() {
+	var vars = {};
+	var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m,key,value) {
+	vars[key] = value;
+	});
+	return vars;
+}  
+
+
+angular
+    .module('NUSAP')
+    .controller('AcadPlannerCtrl', ['$scope', '$http', AcadPlannerCtrl]);
+
+function AcadPlannerCtrl($scope, $http) {
+     
+    var req = {
+			method : 'GET',
+			url    : 'index.php?id=modTaken&token=' + token + '&studentID=' + sessionStorage["netid"];
+		}
+		$http(req).then(
+			function (response) { //success
+                for(var x in response.data.Results.length){
+				    $scope.moduleCode = response.data.Results[x].ModuleCode; 
+                    $scope.moduleTitle = response.data.Results[x].ModuleTitle; 
+                    $scope.acadYear = response.data.Results[x].AcadYear;
+                    $scope.semester = response.data.Results[x].Semester;
+				    $scope.module = $scope.moduleCode + '  ' + $scope.moduleTitle;
+                }
+            }, function (response) { //error
+			}
+    	);
+    };  
+    
+    $scope.moduleTaken = function() {
+    	  
+    };	  
+
+    
+}
